71cf9ed258bc4f722eab4b6e0da38f45e52d52dd
artilleur <99259153+artilleur@users.noreply.github.com>
2023-11-27 10:05:11 +0100

    27112023

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [liste, setListe] = useState([]);
+  const [element, setElement] = useState(""); // Define 'element' state
+  const [titre, setTitre] = useState("");
+  const handleChangeTitre= (event)=>{
+    setTitre(event.target.value);
+  }
+  const loadData = () => {
+
+    axios.get(
+      `http://api.themoviedb.org/3/search/movie?api_key=f33cd318f5135dba306176c13104506a&query=${titre}`,
+      {
+        headers: {
+          Accept: "application/json"
+        }
+      }
+    ).then(
+      (reponse) => {
+        console.log("les données sont servies...");
+        console.log(reponse.data);
+        setListe(reponse.data.results); // Assuming 'results' contains the array of movies
+      }
+    );
+  };
+
+  useEffect(() => {
+    console.log("Chargement du composant...");
+    loadData();
+  }, []);
+
+  const handleChange = (event) => {
+    setElement(event.target.value); // Set 'element' state on input change
+  };
+
+  const handleClick = () => {
+    axios
+      .get("http://api.themoviedb.org/3/search/movie", {
+        params: {
+          api_key: "f33cd318f5135dba306176c13104506a",
+          query: element // Use the 'element' state here
+        }
+      })
+      .then((data) => {
+        console.log(data);
+        loadData();
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <input type="text" value={element} onChange={handleChange} /> */}
+      <button onClick={handleClick}>rechercher</button>
+      <input value={titre} onChange={handleChangeTitre} />
+      <div>
+        {liste.map((movie) => (
+          <div key={movie.id}>
+            {movie.title}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
